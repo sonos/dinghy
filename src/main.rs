547bd21e2eee1d7053e630062ce9c353547dbf7b
Mathieu Poumeyrol <kali@zoy.org>
2016-11-15 18:42:47 +0100

    split mobiledevice-sys out of the way

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![allow(non_camel_case_types)]
-extern crate core_foundation;
+extern crate core_foundation as cf;
 extern crate core_foundation_sys;
 #[macro_use]
 extern crate error_chain;
 extern crate libc;
 extern crate tempdir;
+
+extern crate mobiledevice_sys as mds;
 
 mod errors;
 
@@ -14,88 +16,13 @@ use std::fs;
 use std::path;
 use std::ptr;
 
+use cf::base::{ CFType, CFTypeRef, TCFType };
+use cf::string::CFString;
+use cf::dictionary::{ CFDictionary, CFDictionaryRef };
+
 use libc::*;
 
 use errors::*;
-
-use core_foundation::runloop;
-use core_foundation::base::*;
-use core_foundation::dictionary::{CFDictionary, CFDictionaryRef};
-use core_foundation::string::{CFString, CFStringRef};
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub struct am_device {
-    unknown0: [c_char; 16],
-    device_id: c_int,
-    product_id: c_int,
-    serial: *const c_char,
-    unknown1: c_int,
-    unknown2: [c_char; 4],
-    lockdown_conn: c_int,
-    unknown3: [c_char; 8],
-}
-
-const ADNCI_MSG_CONNECTED: c_uint = 1;
-const ADNCI_MSG_DISCONNECTED: c_uint = 2;
-const ADNCI_MSG_UNSUBSCRIBED: c_uint = 3;
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub struct am_device_notification_callback_info {
-    pub dev: *mut am_device,
-    pub msg: ::std::os::raw::c_uint,
-    pub subscription: *mut am_device_notification,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct am_device_notification {
-    unknown0: c_int,
-    unknown1: c_int,
-    unknown2: c_int,
-    callback: *const am_device_notification_callback,
-    unknown3: c_int,
-}
-
-pub type am_device_notification_callback = extern "C" fn(*mut am_device_notification_callback_info,
-                                                         *mut c_void);
-
-
-pub type am_device_mount_callback = extern "C" fn(CFDictionaryRef, *mut c_void);
-#[link(name = "MobileDevice", kind = "framework")]
-extern "C" {
-    fn AMDeviceNotificationSubscribe(callback: am_device_notification_callback,
-                                     unused0: c_uint,
-                                     unused1: c_uint,
-                                     dn_unknown3: *const c_void,
-                                     notification: *mut *const am_device_notification)
-                                     -> c_int;
-    fn AMDeviceCopyValue(device: *const am_device,
-                         domain: CFStringRef,
-                         cfstring: CFStringRef)
-                         -> *const c_void;
-    fn AMDeviceConnect(device: *const am_device) -> c_int;
-    fn AMDeviceDisconnect(device: *const am_device) -> c_int;
-
-    fn AMDeviceIsPaired(device: *const am_device) -> c_int;
-    fn AMDeviceValidatePairing(device: *const am_device) -> c_int;
-    fn AMDeviceStartSession(device: *const am_device) -> c_int;
-    fn AMDeviceStopSession(device: *const am_device) -> c_int;
-
-    fn AMDeviceMountImage(device: *const am_device,
-                          image: CFStringRef,
-                          options: CFDictionaryRef,
-                          callback: am_device_mount_callback,
-                          cbarg: c_int)
-                          -> c_int;
-
-    fn AMDeviceStartService(device: *const am_device,
-                            service_name: CFStringRef,
-                            socket_fd: *mut c_int,
-                            unknown: *const c_int)
-                            -> c_int;
-}
 
 macro_rules! mk_result {
     ($e:expr) => {{
@@ -117,11 +44,11 @@ fn main() {
 }
 
 fn device_thread() {
-    let notify: *const am_device_notification = std::ptr::null();
+    let notify: *const mds::am_device_notification = std::ptr::null();
     unsafe {
-        AMDeviceNotificationSubscribe(device_callback, 0, 0, ptr::null(), &mut notify.into());
+        mds::AMDeviceNotificationSubscribe(device_callback, 0, 0, ptr::null(), &mut notify.into());
     }
-    runloop::CFRunLoop::run_current();
+    cf::runloop::CFRunLoop::run_current();
 }
 
 #[derive(Clone,Debug)]
@@ -132,13 +59,11 @@ enum Value {
     Boolean(bool),
 }
 
-const MDERR_OK: c_int = 0;
-
 fn rustify(raw: CFTypeRef) -> Result<Value> {
-    use core_foundation::base::TCFType;
-    use core_foundation::data::CFData;
-    use core_foundation::number::CFNumber;
-    use core_foundation::boolean::CFBoolean;
+    use cf::base::TCFType;
+    use cf::data::CFData;
+    use cf::number::CFNumber;
+    use cf::boolean::CFBoolean;
     use core_foundation_sys::number::kCFBooleanTrue;
 
     unsafe {
@@ -166,10 +91,10 @@ fn rustify(raw: CFTypeRef) -> Result<Value> {
     }
 }
 
-fn device_read_value(dev: *const am_device, key: &str) -> Result<Option<Value>> {
+fn device_read_value(dev: *const mds::am_device, key: &str) -> Result<Option<Value>> {
     unsafe {
         let key = CFString::new(key);
-        let raw = AMDeviceCopyValue(dev, ptr::null(), key.as_concrete_TypeRef());
+        let raw = mds::AMDeviceCopyValue(dev, ptr::null(), key.as_concrete_TypeRef());
         if raw.is_null() {
             return Ok(None);
         }
@@ -183,7 +108,7 @@ fn xcode_dev_path() -> Result<path::PathBuf> {
     Ok(String::from_utf8(command.stdout)?.trim().into())
 }
 
-fn device_support_path(dev: *const am_device) -> Result<Option<path::PathBuf>> {
+fn device_support_path(dev: *const mds::am_device) -> Result<Option<path::PathBuf>> {
     let prefix = xcode_dev_path()?.join("Platforms/iPhoneOS.platform/DeviceSupport");
     let os_version = device_read_value(dev, "ProductVersion")?.ok_or("Could not get OS version")?;
     let two_token_version: String = if let Value::String(v) = os_version {
@@ -203,7 +128,7 @@ fn device_support_path(dev: *const am_device) -> Result<Option<path::PathBuf>> {
     Ok(None)
 }
 
-fn mount_developper_image(dev: *const am_device) -> Result<()> {
+fn mount_developper_image(dev: *const mds::am_device) -> Result<()> {
     use std::io::Read;
     unsafe {
         let ds_path = device_support_path(dev)?.ok_or("No device support found in xcode")?;
@@ -211,15 +136,15 @@ fn mount_developper_image(dev: *const am_device) -> Result<()> {
         let sig_image_path = ds_path.join("DeveloperDiskImage.dmg.signature");
         let mut sig: Vec<u8> = vec![];
         fs::File::open(sig_image_path)?.read_to_end(&mut sig);
-        let sig = core_foundation::data::CFData::from_buffer(&*sig);
+        let sig = cf::data::CFData::from_buffer(&*sig);
 
-        use core_foundation::dictionary::CFDictionary;
-        use core_foundation::string::CFString;
+        use cf::dictionary::CFDictionary;
+        use cf::string::CFString;
         let options = [(CFString::from_static_string("ImageType"),
                         CFString::from_static_string("Developper").as_CFType()),
                        (CFString::from_static_string("ImageSignature"), sig.as_CFType())];
         let options = CFDictionary::from_CFType_pairs(&options);
-        let r = AMDeviceMountImage(dev,
+        let r = mds::AMDeviceMountImage(dev,
                            CFString::new(image_path.to_str().unwrap()).as_concrete_TypeRef(),
                            options.as_concrete_TypeRef(),
                            mount_callback,
@@ -244,18 +169,18 @@ extern "C" fn mount_callback(dict: CFDictionaryRef, _arg: *mut c_void) {
 
 }
 
-fn start_remote_debug_server(dev: *const am_device, url: &str) -> Result<c_int> {
+fn start_remote_debug_server(dev: *const mds::am_device, url: &str) -> Result<c_int> {
     unsafe {
-        mk_result!(AMDeviceConnect(dev))?;
-        println!("Pairing ? {}", AMDeviceIsPaired(dev));
-        mk_result!(AMDeviceValidatePairing(dev))?;
+        mk_result!(mds::AMDeviceConnect(dev))?;
+        println!("Pairing ? {}", mds::AMDeviceIsPaired(dev));
+        mk_result!(mds::AMDeviceValidatePairing(dev))?;
         println!("Start Session...");
-        mk_result!(AMDeviceStartSession(dev))?;
+        mk_result!(mds::AMDeviceStartSession(dev))?;
         println!("Mount image...");
         mount_developper_image(dev)?;
         println!("Start debug service...");
         let mut fd: c_int = 0;
-        mk_result!(AMDeviceStartService(dev,
+        mk_result!(mds::AMDeviceStartService(dev,
                                         CFString::from_static_string("com.apple.debugserver")
                                             .as_concrete_TypeRef(),
                                         &mut fd,
@@ -302,7 +227,7 @@ fn start_lldb_proxy(fd: c_int) -> Result<u16> {
     Ok(addr.port())
 }
 
-fn launch_lldb(dev: *const am_device, proxy_port:u16) -> Result<()> {
+fn launch_lldb(dev: *const mds::am_device, proxy_port:u16) -> Result<()> {
     use std::process::Command;
     use std::io::Write;
     let dir = tempdir::TempDir::new("mobiledevice-rs-lldb")?;
@@ -332,16 +257,16 @@ fn launch_lldb(dev: *const am_device, proxy_port:u16) -> Result<()> {
     Ok(())
 }
 
-fn run_remote(dev: *const am_device) -> Result<()> {
+fn run_remote(dev: *const mds::am_device) -> Result<()> {
     let fd = start_remote_debug_server(dev, "")?;
     let proxy = start_lldb_proxy(fd)?;
     launch_lldb(dev, proxy)?;
     Ok(())
 }
 
-extern "C" fn device_callback(info: *mut am_device_notification_callback_info, _arg: *mut c_void) {
+extern "C" fn device_callback(info: *mut mds::am_device_notification_callback_info, _arg: *mut c_void) {
     let device = unsafe { (*info).dev };
-    unsafe { AMDeviceConnect(device) };
+    unsafe { mds::AMDeviceConnect(device) };
     /*
     let properties = ["ActivationPublicKey",
                       "ActivationState",
