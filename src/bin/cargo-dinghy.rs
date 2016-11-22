c9536f2b98406252456488c46d190fa222f16321
Mathieu Poumeyrol <kali@zoy.org>
2016-11-22 14:25:38 +0100

    device listing and selection

--- src/bin/cargo-dinghy.rs
+++ src/bin/cargo-dinghy.rs
@@ -21,6 +21,8 @@ fn main() {
 
     let matches = clap_app!(dinghy =>
         (@arg TARGET: --target +takes_value "target triple (rust convention)")
+        (@arg DEVICE: --device +takes_value "device hint")
+        (@subcommand devices =>)
         (@subcommand test =>
         )
         (@subcommand run =>
@@ -43,9 +45,24 @@ fn main() {
 fn run(matches: clap::ArgMatches) -> Result<()> {
     let dinghy = dinghy::Dinghy::default();
     thread::sleep(Duration::from_millis(100));
-    let d = dinghy.devices().unwrap().pop().ok_or("No phone found")?;
+    let mut devices = dinghy.devices()?.into_iter().filter(|d|
+        match matches.value_of("DEVICE") {
+            Some(filter) => format!("{:?}", d).to_lowercase().contains(&filter.to_lowercase()),
+            None => true,
+    }).collect::<Vec<_>>();
+    if devices.len() == 0 {
+        Err("No devices found")?
+    }
+    let d:Box<dinghy::Device> = devices.remove(0);
     let target = matches.value_of("TARGET").map(|s| s.to_string()).unwrap_or(d.target());
     match matches.subcommand() {
+        ("devices", Some(_matches)) => {
+            let devices = dinghy.devices()?;
+            for d in devices {
+                println!("{:?}", d);
+            }
+            Ok(())
+        }
         ("run", Some(_matches)) => {
             let bin = dinghy::build::compile_bin(&*target)?.pop().expect("no executable");
             let app = d.make_app(&*bin, Some(&*target))?;
