d70356f43d7501e71c2a24afbb204de04b21e3dc
Mathieu Poumeyrol <kali@zoy.org>
2017-01-25 15:05:01 +0100

    setup some actual tests

---
+++ .travis.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+if [ `uname` == Darwin ]
+then
+    (xcrun simctl list devices | grep Booted) || xcrun simctl boot "iPhone 6"
+    rustup target install aarch64-apple-ios
+fi
+
+cargo build --verbose
+cargo test --verbose
+
+cd test-app
+cargo test -- works
+! cargo test -- fails
+echo "## last failure was expected ##"
+../target/debug/cargo-dinghy dinghy test -- works
+! ../target/debug/cargo-dinghy dinghy test -- fails
+echo "## last failure was expected ##"
