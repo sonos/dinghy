2fabb8acb3fdc7cb1ebc2b6f44db5017f94c6cf2
Mathieu Poumeyrol <kali@zoy.org>
2017-01-25 15:15:39 +0100

    don't actually test on linux

--- .travis.sh
+++ .travis.sh
@@ -16,6 +16,9 @@ cd test-app
 cargo test -- works
 ! cargo test -- fails
 echo "## last failure was expected ##"
-../target/debug/cargo-dinghy dinghy test -- works
-! ../target/debug/cargo-dinghy dinghy test -- fails
-echo "## last failure was expected ##"
+if [ `uname` = Darwin ]
+then
+    ../target/debug/cargo-dinghy dinghy test -- works
+    ! ../target/debug/cargo-dinghy dinghy test -- fails
+    echo "## last failure was expected ##"
+fi
