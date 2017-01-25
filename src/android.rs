279d97c8327dd743af3dfc9f4c0f6027ebee9b5d
David Hewson <dev@daveid.co.uk>
2017-01-25 20:01:30 +0000

    accept any android name that isn't whitespace
    otherwise it won't try 192.168.1.1:5555 when connected over tcpip

--- src/android.rs
+++ src/android.rs
@@ -77,7 +77,7 @@ impl PlatformManager for AndroidManager {
     fn devices(&self) -> Result<Vec<Box<Device>>> {
         let result = Command::new("adb").arg("devices").output()?;
         let mut devices = vec![];
-        let device_regex = ::regex::Regex::new(r#"^(\w+)\tdevice\r?$"#)?;
+        let device_regex = ::regex::Regex::new(r#"^(\S+)\tdevice\r?$"#)?;
         for line in String::from_utf8(result.stdout)?.split("\n").skip(1) {
             if let Some(caps) = device_regex.captures(line) {
                 let d = AndroidDevice::from_id(&caps[1])?;
