888c01f2d5868ac50eceeae0c6b898e19e0f52fb
Mathieu Poumeyrol <kali@users.noreply.github.com>
2017-01-31 09:26:02 +0100

    Support for multiple travis configurations (#8)
    * extends os coverage
    
    * extends os coverage
    
    * setup some actual tests
    
    * shell syntax error
    
    * path to cargo
    
    * don't actually test on linux
    
    * missing openssl on mac
    
    * missing openssl on mac
    
    * install the right toochain
    
    * hopefully trigger travis
    
    * pip install six
    
    * test three channels on linux
    
    * test three channels on linux
    
    * try adding android to the mix
    
    * try adding android to the mix
    
    * try adding android to the mix
    
    * android even worse than ios

---
+++ .travis.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+export PATH=$PATH:$HOME/.cargo/bin
+RUST_VERSION=${RUST_VERSION:=stable}
+
+rustup install $RUST_VERSION
+rustup override set $RUST_VERSION
+
+if [ `uname` = Darwin ]
+then
+    (xcrun simctl list devices | grep Booted) || xcrun simctl boot "iPhone 6"
+    rustup target install x86_64-apple-ios
+    pip install six
+    export OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include
+    export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
+fi
+
+cargo build --verbose
+cargo test --verbose
+
+cd test-app
+cargo test -- works
+! cargo test -- fails
+echo "## last failure was expected ##"
+
+if [ `uname` = Darwin ]
+then
+    ../target/debug/cargo-dinghy dinghy test -- works
+    ! ../target/debug/cargo-dinghy dinghy test -- fails
+    echo "## last failure was expected ##"
+fi
---
+++ test-app/src/lib.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+
+    #[test]
+    fn it_fails() {
+        panic!("Failing as expected");
+    }
+}
