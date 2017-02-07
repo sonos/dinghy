40738b67083d8a86abe1a4ebdfd4b6ef71d987e7
Mathieu Poumeyrol <kali@zoy.org>
2017-02-07 21:12:46 +0100

    debug travis.sh

--- .travis.sh
+++ .travis.sh
@@ -21,14 +21,17 @@ fi
 cargo build --verbose
 cargo test --verbose
 
+set -x
+
 cd test-app
 cargo test -- works
 ! cargo test -- fails
 echo "## last failure was expected ##"
 
+
 if [ `uname` = Darwin ]
 then
-    ../target/debug/cargo-dinghy dinghy test -- pass
-    ! ../target/debug/cargo-dinghy dinghy test -- fails
+    ../target/debug/cargo-dinghy dinghy test pass
+    ! ../target/debug/cargo-dinghy dinghy test fails
     echo "## last failure was expected ##"
 fi
