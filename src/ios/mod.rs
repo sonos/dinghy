9624d3a3318ea2daf702be5badc2ee9da81c2411
Andy Lowry <andy@andylowry.me.uk>
2017-06-27 14:52:01 +0100

    More ios buil fix

--- src/ios/mod.rs
+++ src/ios/mod.rs
@@ -117,7 +117,7 @@ impl Device for IosDevice {
         let lldb_proxy = self.start_remote_lldb()?;
         run_remote(self.ptr, &lldb_proxy, app_path, args, false)
     }
-    fn debug_app(&self, app_path: &path::Path, args: &[&str]) -> Result<()> {
+    fn debug_app(&self, app_path: &path::Path, args: &[&str], _envs: &[&str]) -> Result<()> {
         let lldb_proxy = self.start_remote_lldb()?;
         run_remote(self.ptr, &lldb_proxy, app_path, args, true)
     }
@@ -188,7 +188,7 @@ impl Device for IosSimDevice {
         let install_path = String::from_utf8(process::Command::new("xcrun").args(&["simctl", "get_app_container", &self.id, "Dinghy"]).output()?.stdout)?;
         launch_lldb_simulator(&self, &*install_path, args, false)
     }
-    fn debug_app(&self, _app_path: &path::Path, args: &[&str]) -> Result<()> {
+    fn debug_app(&self, _app_path: &path::Path, args: &[&str], _envs: &[&str]) -> Result<()> {
         let install_path = String::from_utf8(process::Command::new("xcrun").args(&["simctl", "get_app_container", &self.id, "Dinghy"]).output()?.stdout)?;
         launch_lldb_simulator(&self, &*install_path, args, true)
     }
