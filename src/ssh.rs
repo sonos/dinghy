9f25c6afe6fe9f59c102c25dc8be0a1d561ac61e
Andy Lowry <andy@andylowry.me.uk>
2017-07-03 18:20:43 +0100

    Don't pass port if not specified So it can be set in ~/.ssh/config

--- src/ssh.rs
+++ src/ssh.rs
@@ -29,18 +29,31 @@ impl Device for SshDevice {
     fn install_app(&self, app: &path::Path) -> Result<()> {
         let user_at_host = format!("{}@{}", self.config.username, self.config.hostname);
         let prefix = self.config.path.clone().unwrap_or("/tmp".into());
-        let _stat = process::Command::new("ssh")
-            .args(
-                &[
-                    &*user_at_host,
-                    "-p",
-                    &*format!("{}", self.config.port.unwrap_or(22)),
-                    "mkdir",
-                    "-p",
-                    &*format!("{}/dinghy", prefix),
-                ],
-            )
-            .status();
+        let _stat = if let Some(port) = self.config.port {
+            process::Command::new("ssh")
+                .args(
+                    &[
+                        &*user_at_host,
+                        "-p",
+                        &*format!("{}", port),
+                        "mkdir",
+                        "-p",
+                        &*format!("{}/dinghy", prefix),
+                    ],
+                )
+                .status()
+        } else {
+            process::Command::new("ssh")
+                .args(
+                    &[
+                        &*user_at_host,
+                        "mkdir",
+                        "-p",
+                        &*format!("{}/dinghy", prefix),
+                    ],
+                )
+                .status()
+        };
         let target_path = format!(
             "{}/dinghy/{}",
             prefix,
@@ -53,14 +66,23 @@ impl Device for SshDevice {
             user_at_host,
             &*target_path
         );
-        let stat = process::Command::new("/usr/bin/rsync")
-            .arg("-a")
-            .arg("-v")
-            .arg("-e")
-            .arg(&*format!("ssh -p {}", self.config.port.unwrap_or(22)))
-            .arg(&*format!("{}/", app.to_str().unwrap()))
-            .arg(&*format!("{}:{}/", user_at_host, &*target_path))
-            .status()?;
+        let stat = if let Some(port) = self.config.port {
+            process::Command::new("/usr/bin/rsync")
+                .arg("-a")
+                .arg("-v")
+                .arg("-e")
+                .arg(&*format!("ssh -p {}", port))
+                .arg(&*format!("{}/", app.to_str().unwrap()))
+                .arg(&*format!("{}:{}/", user_at_host, &*target_path))
+                .status()?
+        } else {
+            process::Command::new("/usr/bin/rsync")
+                .arg("-a")
+                .arg("-v")
+                .arg(&*format!("{}/", app.to_str().unwrap()))
+                .arg(&*format!("{}:{}/", user_at_host, &*target_path))
+                .status()?
+        };
         if !stat.success() {
             Err("error installing app")?
         }
@@ -70,18 +92,20 @@ impl Device for SshDevice {
         let user_at_host = format!("{}@{}", self.config.username, self.config.hostname);
         let prefix = self.config.path.clone().unwrap_or("/tmp".into());
         let app_name = app_path.file_name().unwrap();
-        let path = path::PathBuf::from(prefix)
-            .join("dinghy")
-            .join(app_name);
-        let stat = process::Command::new("ssh")
-            .arg(user_at_host)
-            .arg("-p")
-            .arg(&*format!("{}", self.config.port.unwrap_or(22)))
-            .arg(&*format!(
-                "rm -rf {}",
-                &path.to_str().unwrap()
-            ))
-            .status()?;
+        let path = path::PathBuf::from(prefix).join("dinghy").join(app_name);
+        let stat = if let Some(port) = self.config.port {
+            process::Command::new("ssh")
+                .arg(user_at_host)
+                .arg("-p")
+                .arg(&*format!("{}", port))
+                .arg(&*format!("rm -rf {}", &path.to_str().unwrap()))
+                .status()?
+        } else {
+            process::Command::new("ssh")
+                .arg(user_at_host)
+                .arg(&*format!("rm -rf {}", &path.to_str().unwrap()))
+                .status()?
+        };
         if !stat.success() {
             Err("test fail.")?
         }
@@ -91,21 +115,31 @@ impl Device for SshDevice {
         let user_at_host = format!("{}@{}", self.config.username, self.config.hostname);
         let prefix = self.config.path.clone().unwrap_or("/tmp".into());
         let app_name = app_path.file_name().unwrap();
-        let path = path::PathBuf::from(prefix)
-            .join("dinghy")
-            .join(app_name);
+        let path = path::PathBuf::from(prefix).join("dinghy").join(app_name);
         let exe = path.join(&app_name);
-        let stat = process::Command::new("ssh")
-            .arg(user_at_host)
-            .arg("-p")
-            .arg(&*format!("{}", self.config.port.unwrap_or(22)))
-            .arg(&*format!(
-                "DINGHY=1 {} {}",
-                envs.join(" "),
-                &exe.to_str().unwrap()
-            ))
-            .args(args)
-            .status()?;
+        let stat = if let Some(port) = self.config.port {
+            process::Command::new("ssh")
+                .arg(user_at_host)
+                .arg("-p")
+                .arg(&*format!("{}", port))
+                .arg(&*format!(
+                    "DINGHY=1 {} {}",
+                    envs.join(" "),
+                    &exe.to_str().unwrap()
+                ))
+                .args(args)
+                .status()?
+        } else {
+            process::Command::new("ssh")
+                .arg(user_at_host)
+                .arg(&*format!(
+                    "DINGHY=1 {} {}",
+                    envs.join(" "),
+                    &exe.to_str().unwrap()
+                ))
+                .args(args)
+                .status()?
+        };
         if !stat.success() {
             Err("test fail.")?
         }
