d7100f7d426ea8df243c9781b1717e1fd3274010
ratamovic <ratamovic@gmail.com>
2017-12-19 18:14:34 +0100

    Print env variable and return proper error if toolchain not found

--- src/regular_platform.rs
+++ src/regular_platform.rs
@@ -1,6 +1,7 @@
 use std::{env, path};
 
 use {Result, Platform};
+use std::ffi::OsStr;
 use itertools::Itertools;
 use walkdir::WalkDir;
 
@@ -16,14 +17,16 @@ pub struct RegularPlatform {
 
 impl RegularPlatform {
     pub fn new<P: AsRef<path::Path>>(id:String, rustc_triple:String, toolchain: P) -> Result<Box<Platform>> {
+        let toolchain_path = toolchain.as_ref();
         let mut bin: Option<path::PathBuf> = None;
         let mut prefix: Option<String> = None;
-        for file in toolchain.as_ref().join("bin").read_dir()? {
+
+        for file in toolchain_path.join("bin").read_dir().map_err(|_| format!("Couldn't find toolchain directory {:?}", toolchain_path.display()))? {
             let file = file?;
             if file.file_name().to_string_lossy().ends_with("-gcc")
                 || file.file_name().to_string_lossy().ends_with("-gcc.exe")
             {
-                bin = Some(toolchain.as_ref().join("bin"));
+                bin = Some(toolchain_path.join("bin"));
                 prefix = Some(
                     file.file_name()
                         .to_string_lossy()
@@ -35,10 +38,10 @@ impl RegularPlatform {
         }
         let bin = bin.ok_or("no bin/*-gcc found in toolchain")?;
         let tc_triple = prefix.ok_or("no gcc in toolchain")?.to_string();
-        let sysroot = sysroot_in_toolchain(&toolchain)?;
+        let sysroot = sysroot_in_toolchain(&toolchain_path)?;
         Ok(Box::new(RegularPlatform {
             id,
-            root: toolchain.as_ref().into(),
+            root: toolchain_path.into(),
             bin,
             rustc_triple,
             tc_triple,
@@ -78,40 +81,49 @@ impl Platform for RegularPlatform {
             ::shim::GLOB_ARGS
         ))
     }
+
     fn setup_more_env(&self) -> Result<()> {
+        fn set_env<K: AsRef<OsStr>, V: AsRef<OsStr>>(k: K, v: V) {
+            info!("Setting environment variable {:?}='{:?}'", k.as_ref(), v.as_ref());
+            println!("Setting environment variable {:?}='{:?}'", k.as_ref(), v.as_ref());
+            env::set_var(k, v);
+        }
+
         let wd_path = ::cargo::util::important_paths::find_root_manifest_for_wd(None, &env::current_dir()?)?;
         let root = wd_path.parent().ok_or("building at / ?")?;
         let path = env::var("PATH").unwrap();
         let shims_path = root.join("target").join(&self.rustc_triple).join(&self.id);
 
-        env::set_var("PATH", format!("{}:{}", path, shims_path.to_string_lossy()));
+        set_env("PATH", format!("{}:{}", path, shims_path.to_string_lossy()));
         for exe in self.bin.read_dir()? {
             let exe = exe?;
             let rustified_exe = &exe.file_name().to_string_lossy().replace(&self.tc_triple, &self.rustc_triple);
             println!("toolchain: {} -> {}", exe.path().to_string_lossy(), rustified_exe);
+            info!("toolchain: {} -> {}", exe.path().to_string_lossy(), rustified_exe);
             ::shim::create_shim(root, &self.rustc_triple, &self.id, rustified_exe, &format!("{} {}", exe.path().to_string_lossy(), ::shim::GLOB_ARGS))?;
         }
-        env::set_var("TARGET_SYSROOT", &self.sysroot.clone());
-        env::set_var("TARGET_AR", &self.binary("ar"));
-        env::set_var("PKG_CONFIG_ALLOW_CROSS", "1");
-        env::set_var("PKG_CONFIG_LIBDIR", WalkDir::new(self.root.to_string_lossy().as_ref())
+        set_env("TARGET_SYSROOT", &self.sysroot.clone());
+        set_env("TARGET_AR", &self.binary("ar"));
+        set_env("PKG_CONFIG_ALLOW_CROSS", "1");
+        set_env("PKG_CONFIG_LIBDIR", WalkDir::new(self.root.to_string_lossy().as_ref())
             .into_iter()
             .filter_map(|e| e.ok()) // Ignore unreadable files, maybe could warn...
             .filter(|e| e.file_name() == "pkgconfig" && e.file_type().is_dir())
             .map(|e| e.path().to_string_lossy().into_owned())
             .join(":"));
-        env::set_var("PKG_CONFIG_SYSROOT_DIR", &self.sysroot.clone());
+        set_env("PKG_CONFIG_SYSROOT_DIR", &self.sysroot.clone());
         Ok(())
     }
 }
 
-fn sysroot_in_toolchain<P: AsRef<path::Path>>(p: P) -> Result<String> {
-    let immediate = p.as_ref().join("sysroot");
+fn sysroot_in_toolchain<P: AsRef<path::Path>>(toolchain_path: P) -> Result<String> {
+    let toolchain = toolchain_path.as_ref();
+    let immediate = toolchain.join("sysroot");
     if immediate.is_dir() {
         let sysroot = immediate.to_str().ok_or("sysroot is not utf-8")?;
         return Ok(sysroot.into());
     }
-    for subdir in p.as_ref().read_dir()? {
+    for subdir in toolchain.read_dir()? {
         let subdir = subdir?;
         let maybe = subdir.path().join("sysroot");
         if maybe.is_dir() {
@@ -119,5 +131,5 @@ fn sysroot_in_toolchain<P: AsRef<path::Path>>(p: P) -> Result<String> {
             return Ok(sysroot.into());
         }
     }
-    Err(format!("no sysroot found in toolchain {:?}", p.as_ref()))?
+    Err(format!("no sysroot found in toolchain {:?}", toolchain))?
 }
