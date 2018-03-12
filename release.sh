ad95fcf5b88b2f57ddf5e36f35e03c05fc7b7bff
Mathieu Poumeyrol <kali@zoy.org>
2018-03-12 14:21:58 +0100

    release management scripts

---
+++ release.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+CRATE=$1
+VERSION=$2
+CRATES="dinghy-build dinghy-test dinghy-lib cargo-dinghy"
+
+if [ -z "$VERSION" ]
+then
+    echo "Usage: $0 <crate> <version>" 
+    echo crates order is: $CRATES
+    exit 1
+fi
+
+# set_version cargo-dinghy/Cargo.toml 0.3.0
+set_version() {
+    FILE=$1
+    VERSION=$2
+    sed -i.back "s/^version *= *\".*\"/version = \"$2\"/" $FILE
+    sed -i.back "s/^\(dinghy-[^ =]*\).*/\\1 = \"$2\"/" $FILE
+}
+
+set -ex
+
+set_version $CRATE/Cargo.toml $VERSION
+(cd $CRATE ; cargo publish --dry-run --allow-dirty)
+
+git commit . -m "release $CRATE/$VERSION"
+git tag "$CRATE/$VERSION"
+
+(cd $CRATE ; cargo publish)
