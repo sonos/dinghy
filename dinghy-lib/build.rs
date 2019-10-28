ffbea600f45d79782fbc38feaf07562195f44b8c
Mathieu Poumeyrol <kali@zoy.org>
2019-10-28 09:16:48 +0100

    add a "private" framework search path

--- dinghy-lib/build.rs
+++ dinghy-lib/build.rs
@@ -1,6 +1,7 @@
 #[cfg(target_os = "macos")]
 fn main() {
     println!("cargo:rustc-link-search=framework=/System/Library/PrivateFrameworks");
+    println!("cargo:rustc-link-search=framework=/Library/Apple/System/Library/PrivateFrameworks");
 }
 
 #[cfg(not(target_os = "macos"))]
