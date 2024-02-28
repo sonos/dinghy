8702e28623f30b507dbd55b49a0a434cffa10f85
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-02-28 13:43:37 +0100

    ios-deploy is back. improved feedback and ergonomics a bit

--- dinghy-lib/src/apple/mod.rs
+++ dinghy-lib/src/apple/mod.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::fmt::Display;
 
-pub use self::device::{IosDevice, AppleSimDevice};
+pub use self::device::{AppleSimDevice, IosDevice};
 pub use self::platform::AppleDevicePlatform;
 use crate::{Device, Platform, PlatformManager, Result};
 use itertools::Itertools;
@@ -9,7 +10,7 @@ mod device;
 mod platform;
 mod xcode;
 
-use anyhow::{anyhow, Context, bail};
+use anyhow::{anyhow, bail, Context};
 use log::info;
 
 #[derive(Debug, Clone)]
@@ -56,9 +57,9 @@ impl IosManager {
             .into_iter()
             .chain(
                 simulators(AppleSimulatorType::Ios)
-                .context("Could not list iOS simulators")?
-                .into_iter(),
-                )
+                    .context("Could not list iOS simulators")?
+                    .into_iter(),
+            )
             .collect();
         Ok(Some(IosManager { devices }))
     }
@@ -78,29 +79,29 @@ impl PlatformManager for IosManager {
             "x86_64",
             "aarch64-sim",
         ]
-            .iter()
-            .map(|arch| {
-                let id = format!("auto-ios-{}", arch);
-                let rustc_triple = if *arch != "aarch64-sim" {
-                    format!("{}-apple-ios", arch)
-                } else {
-                    format!("aarch64-apple-ios-sim")
-                };
+        .iter()
+        .map(|arch| {
+            let id = format!("auto-ios-{}", arch);
+            let rustc_triple = if *arch != "aarch64-sim" {
+                format!("{}-apple-ios", arch)
+            } else {
+                format!("aarch64-apple-ios-sim")
+            };
 
-                let simulator = if *arch == "x86_64" || *arch == "aarch64-sim" {
-                    Some(AppleSimulatorType::Ios)
-                } else {
-                    None
-                };
+            let simulator = if *arch == "x86_64" || *arch == "aarch64-sim" {
+                Some(AppleSimulatorType::Ios)
+            } else {
+                None
+            };
 
-                AppleDevicePlatform::new(
-                    id,
-                    &rustc_triple,
-                    simulator,
-                    crate::config::PlatformConfiguration::default(),
-                    )
-                    .map(|pf| pf as Box<dyn Platform>)
-            })
+            AppleDevicePlatform::new(
+                id,
+                &rustc_triple,
+                simulator,
+                crate::config::PlatformConfiguration::default(),
+            )
+            .map(|pf| pf as Box<dyn Platform>)
+        })
         .collect()
     }
 }
@@ -121,12 +122,7 @@ impl PlatformManager for WatchosManager {
     }
 
     fn platforms(&self) -> Result<Vec<Box<dyn Platform>>> {
-        [
-            "arm64_32",
-            "aarch64",
-            "x86_64-sim",
-            "aarch64-sim",
-        ]
+        ["arm64_32", "aarch64", "x86_64-sim", "aarch64-sim"]
             .iter()
             .map(|arch| {
                 let id = format!("auto-watchos-{}", arch);
@@ -151,10 +147,10 @@ impl PlatformManager for WatchosManager {
                     &rustc_triple,
                     simulator,
                     crate::config::PlatformConfiguration::default(),
-                    )
-                    .map(|pf| pf as Box<dyn Platform>)
+                )
+                .map(|pf| pf as Box<dyn Platform>)
             })
-        .collect()
+            .collect()
     }
 }
 
@@ -175,11 +171,7 @@ impl PlatformManager for TvosManager {
     }
 
     fn platforms(&self) -> Result<Vec<Box<dyn Platform>>> {
-        [
-            "aarch64",
-            "x86_64",
-            "aarch64-sim",
-        ]
+        ["aarch64", "x86_64", "aarch64-sim"]
             .iter()
             .map(|arch| {
                 let id = format!("auto-tvos-{}", arch);
@@ -198,16 +190,22 @@ impl PlatformManager for TvosManager {
                     &rustc_triple,
                     simulator,
                     crate::config::PlatformConfiguration::default(),
-                    )
-                    .map(|pf| pf as Box<dyn Platform>)
+                )
+                .map(|pf| pf as Box<dyn Platform>)
             })
-        .collect()
+            .collect()
     }
 }
 
 fn simulators(sim_type: AppleSimulatorType) -> Result<Vec<Box<dyn Device>>> {
     let sims_list = ::std::process::Command::new("xcrun")
-        .args(&["simctl", "list", "--json", "devices", sim_type.to_string().as_str()])
+        .args(&[
+            "simctl",
+            "list",
+            "--json",
+            "devices",
+            sim_type.to_string().as_str(),
+        ])
         .output()?;
     if !sims_list.status.success() {
         info!(
@@ -227,12 +225,12 @@ fn simulators(sim_type: AppleSimulatorType) -> Result<Vec<Box<dyn Device>>> {
                         .as_str()
                         .ok_or_else(|| anyhow!("unexpected simulator list format (missing name)"))?
                         .to_string(),
-                        id: sim["udid"]
-                            .as_str()
-                            .ok_or_else(|| anyhow!("unexpected simulator list format (missing udid)"))?
-                            .to_string(),
-                            os: k.split(" ").last().unwrap().to_string(),
-                            sim_type: sim_type.clone(),
+                    id: sim["udid"]
+                        .as_str()
+                        .ok_or_else(|| anyhow!("unexpected simulator list format (missing udid)"))?
+                        .to_string(),
+                    os: k.split(" ").last().unwrap().to_string(),
+                    sim_type: sim_type.clone(),
                 }))
             }
         }
@@ -241,6 +239,16 @@ fn simulators(sim_type: AppleSimulatorType) -> Result<Vec<Box<dyn Device>>> {
 }
 
 fn devices() -> Result<Vec<Box<dyn Device>>> {
+    let mut devices: HashMap<String, IosDevice> = Default::default();
+    devices_from_devicectl(&mut devices)?;
+    devices_from_ios_deploy(&mut devices)?;
+    Ok(devices
+        .into_values()
+        .map(|d| Box::new(d) as _)
+        .collect_vec())
+}
+
+fn devices_from_devicectl(devices: &mut HashMap<String, IosDevice>) -> Result<()> {
     let tempdir = tempdir::TempDir::new("dinghy-ios")?;
     let tmpjson = tempdir.path().join("json");
     let devicectl = std::process::Command::new("xcrun")
@@ -252,46 +260,74 @@ fn devices() -> Result<Vec<Box<dyn Device>>> {
     if !devicectl.status.success() {
         bail!("xcrun command failed. Please check that \"xcrun devicectl list devices\" works.\n{devicectl:?}");
     }
-    let mut devices = vec!();
     for device in json::parse(&std::fs::read_to_string(tmpjson)?)?["result"]["devices"].members() {
+        let udid = device["hardwareProperties"]["udid"]
+            .as_str()
+            .context("no identifier in device json")?
+            .to_string();
         let device = IosDevice::new(
-            device["deviceProperties"]["name"].as_str().context("no name in device json")?.to_string(),
-            device["hardwareProperties"]["udid"].as_str().context("no identifier in device json")?.to_string(),
-            device["hardwareProperties"]["cpuType"]["name"].as_str().context("no cpuType in device json")?
+            device["deviceProperties"]["name"]
+                .as_str()
+                .context("no name in device json")?
+                .to_string(),
+            udid.clone(),
+            device["hardwareProperties"]["cpuType"]["name"]
+                .as_str()
+                .context("no cpuType in device json")?,
+            device["deviceProperties"]["osVersionNumber"]
+                .as_str()
+                .context("no osVersionNumber")?
+                .to_string(),
         )?;
-        devices.push(Box::new(device) as _);
+        devices.insert(udid, device);
     }
-    Ok(devices)
+    Ok(())
 }
 
-/*
-   fn devices() -> Result<Vec<Box<dyn Device>>> {
-   let list = ::std::process::Command::new("ios-deploy")
-   .stderr(std::process::Stdio::inherit())
-   .args(&["-c", "--json", "-t", "1"])
-   .output();
-   let list = match list {
-   Ok(l) => l,
-   Err(e) => {
-   info!(
-   "Could not execute ios-deploy to look for iOS devices ({}), so iOS device support is disabled. Consider installing ios-deploy (`brew install ios-deploy`...) for iOS support.", e);
-   return Ok(vec![]);
-   }
-   };
-   if !list.status.success() {
-   info!(
-   "ios-deploy returned an error while listing devices. It this is not expected, you need to make sure `ios-deploy --json -c -t 1` works as expected."
-   );
-   return Ok(vec![]);
-   }
-// ios-deploy outputs each device as a multiline json dict, with separator or delimiter. make
-// it a json array.
-let list = String::from_utf8(list.stdout)?.replace("}{", "},{");
-let list = format!("[{}]", list);
-let list = ::json::parse(&list)
-.with_context(|| "Could not parse output for: `ios-deploy --json -c -t 1` as json. Please try to make this command work and retry.")?;
-list.members()
-.map(|json| Ok(Box::new(IosDevice::for_ios_deploy_json(&json)?) as Box<dyn Device>))
-.collect::<Result<Vec<Box<dyn Device>>>>()
+fn devices_from_ios_deploy(devices: &mut HashMap<String, IosDevice>) -> Result<()> {
+    let list = ::std::process::Command::new("ios-deploy")
+        .stderr(std::process::Stdio::inherit())
+        .args(&["-c", "--json", "-t", "1"])
+        .output();
+    let list = match list {
+        Ok(l) => l,
+        Err(e) => {
+            info!(
+                "Could not execute ios-deploy to look for legacy (before iOS 17) iOS devices ({}). Consider installing ios-deploy (`brew install ios-deploy`...) for legacy iOS support.", e);
+            return Ok(());
+        }
+    };
+    if !list.status.success() {
+        info!(
+            "ios-deploy returned an error while listing devices. It this is not expected, you need to make sure `ios-deploy --json -c -t 1` works as expected. ios-deploy is needed for pre-ios17 devices."
+            );
+        return Ok(());
+    }
+    // ios-deploy outputs each device as a multiline json dict, with separator or delimiter. make
+    // it a json array.
+    let list = String::from_utf8(list.stdout)?.replace("}{", "},{");
+    let list = format!("[{}]", list);
+    let list = ::json::parse(&list)
+        .with_context(|| "Could not parse output for: `ios-deploy --json -c -t 1` as json. Please try to make this command work and retry.")?;
+    for json in list.members() {
+        let device = &json["Device"];
+        let id = device["DeviceIdentifier"]
+            .as_str()
+            .context("DeviceIdentifier expected to be a string")?
+            .to_owned();
+        let name = device["DeviceName"]
+            .as_str()
+            .context("DeviceName expected to be a string")?
+            .to_owned();
+        let arch_cpu = device["modelArch"].as_str().unwrap_or("arm64");
+        let ios_version = device["ProductVersion"]
+            .as_str()
+            .context("ProductVersion expected to be a string")?
+            .to_string();
+        devices.insert(
+            name.clone(),
+            IosDevice::new(name, id, &arch_cpu, ios_version)?,
+        );
+    }
+    Ok(())
 }
-*/
